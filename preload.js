8dc2171abc344a008f24655b391d8871f40c6291
AKAAKUNLEE <644469786@qq.com>
2025-04-03 18:11:56 +0800

    新增网络控制和事件监听功能，优化扩展管理接口

--- preload.js
+++ preload.js
@@ -1,12 +1,21 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  // 扩展管理
   getExtensions: () => ipcRenderer.invoke('extensions:list'),
   installExtension: (path) => ipcRenderer.invoke('extensions:install', path),
   removeExtension: (id) => ipcRenderer.invoke('extensions:remove', id),
   openExtensionDialog: () => ipcRenderer.invoke('extensions:open-dialog'),
-  showExtensionDetails: (id) => ipcRenderer.send('extension:show-details', id),
-  onExtensionsUpdated: (callback) => {
-    ipcRenderer.on('extensions:updated', (_, extensions) => callback(extensions));
+
+  // 网络控制
+  retryConnection: () => ipcRenderer.invoke('network:retry'),
+  checkConnection: () => ipcRenderer.invoke('network:check'),
+
+  // 开发者工具
+  openDevTools: () => ipcRenderer.send('devtools:open'),
+
+  // 事件监听
+  onConnectionStatus: (callback) => {
+    ipcRenderer.on('connection-status', (_, status) => callback(status));
   }
 });
